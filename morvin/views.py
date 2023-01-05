6b907024dade880ef0c8018f46b746b8dce7e90f
kasirye Nicholas <24528212+kasinico@users.noreply.github.com>
2023-01-05 09:29:31 +0300

    disable cSpell check

--- morvin/views.py
+++ morvin/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from allauth.account.views import PasswordSetView,PasswordChangeView
+from allauth.account.views import PasswordSetView, PasswordChangeView
 from django.urls import reverse, reverse_lazy
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 
@@ -14,19 +14,16 @@ from .forms import EmailComposeForm
 from .forms import ChatMessageForm
 from django.views.generic import DeleteView
 from django.urls import reverse
-from .models import ChatMessage #chatmessages
+from .models import ChatMessage  # chatmessages
 from django.core.mail import send_mail
 from django.core.paginator import Paginator
 
 
+# class Index(LoginRequiredMixin,TemplateView):
+# template_name = "index.html"
 
 
-
-#class Index(LoginRequiredMixin,TemplateView):
-    #template_name = "index.html"
-
-
-#index page Dashboard with counter
+# index page Dashboard with counter
 def index(request):
     context = {}
 
@@ -35,21 +32,27 @@ def index(request):
 
     return render(request, 'index.html', context)
 
-    
-class Calendar(LoginRequiredMixin,TemplateView):
+
+class Calendar(LoginRequiredMixin, TemplateView):
     template_name = "calendar.html"
-#addcrime class
-#class Addcrime(TemplateView):
-    #template_name = "addcrime.html"
-class Chat(LoginRequiredMixin,TemplateView):
+# addcrime class
+# class Addcrime(TemplateView):
+    # template_name = "addcrime.html"
+
+
+class Chat(LoginRequiredMixin, TemplateView):
     template_name = "chat.html"
+
+
 class MyPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
     success_url = reverse_lazy('index')
+
+
 class MyPasswordSetView(LoginRequiredMixin, PasswordSetView):
     success_url = reverse_lazy('index')
 
 
-#logic for add crime input
+# logic for add crime input
 # ============================ Start functions for crud Addcrime ====================
 """def addcrime(request):
     # dictionary for initial data with
@@ -65,34 +68,36 @@ class MyPasswordSetView(LoginRequiredMixin, PasswordSetView):
         form.save()
     return HttpResponse(status=200)
     """
+
+
 def addcrime(request):
     # create a new instance of CrimeModel
     my_model_instance = CrimeModel()
     if request.method == "POST":
-        form = CrimeForm(request.POST, instance=my_model_instance)  
-        if form.is_valid():  
-            form.save()  
-            return redirect('/list_view')  
-    else:  
-        form = CrimeForm(instance=my_model_instance)  
-    return render(request,'addcrime.html',{'form':form})  
- 
+        form = CrimeForm(request.POST, instance=my_model_instance)
+        if form.is_valid():
+            form.save()
+            return redirect('/list_view')
+    else:
+        form = CrimeForm(instance=my_model_instance)
+    return render(request, 'addcrime.html', {'form': form})
 
-#list/show crime view
+
+# list/show crime view
 def list_view(request):
-     # retrieve the object with the given pk
+    # retrieve the object with the given pk
     # dictionary for initial data with
     # field names as keys
-    paginator = Paginator(CrimeModel.objects.all(), 5) # 5 is the number of objects per page
+    # 5 is the number of objects per page
+    paginator = Paginator(CrimeModel.objects.all(), 5)
     page = request.GET.get('page')
     objects = paginator.get_page(page)
-    context ={}
+    context = {}
     context["dataset"] = objects
     return render(request, "list_view.html", context)
 
 
-
-#edit member crime
+# edit member crime
 def edit(request, id):
     my_model_instance = get_object_or_404(CrimeModel, id=id)
     if request.method == 'POST':
@@ -105,23 +110,22 @@ def edit(request, id):
     return render(request, 'edit.html', {'form': form})
 
 
-
-#delete crime row
+# delete crime row
 def delete(request, id):
     crime = get_object_or_404(CrimeModel, pk=id)
     crime.delete()
     return redirect('list_view')
 
-# ============================ End functions for crud Addcrime ====================
+# ============================ End functions for crud addcrime ====================
 
 
-
-#chat message
+# chat message
 # ============================ Start functions for chat message ====================
 
 def chat(request):
     messages = ChatMessage.objects.all()
     return render(request, 'chat.html', {'messages': messages})
+
 
 def chat_view(request):
     # Get all chat messages in the database
@@ -143,5 +147,3 @@ def chat_view(request):
 
     # Render the chat view template, passing the form and chat_messages as context
     return render(request, 'chat.html', {'form': form, 'chat_messages': chat_messages})
-
-
