257aaef507df825cee75c92968f09f33b0024dd0
kasirye Nicholas <24528212+kasinico@users.noreply.github.com>
2024-03-04 22:50:27 +0300

    re modifying css

---
+++ pytests.py
@@ -0,0 +1,13 @@
+
+
+class Test1:
+
+    # @pytest.fixture()
+
+
+    def myfixture(self):
+        print("my fix is called")
+
+
+    def test_method1(self):
+        print("method-1 is called")
--- tests.py
+++ tests.py
@@ -5,25 +5,23 @@
 # http://localhost:8000/account/login/
 
 # when to use find and get_element
-# from selenium import webdriver
-# #import time
-
-# driver = webdriver.Chrome('Users\ELLAH\Downloads\chromedriver')
-# driver.get('http://localhost:8000/account/login')
-
 from selenium import webdriver
-# selenium 3
-from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver = webdriver.Chrome('C:\DRIVERS\chromedriver')
+driver = webdriver.Chrome()
+driver.get('http://localhost:8000/account/login')
+# from selenium import webdriver
+from selenium.webdriver import Chrome
+
+
+# selenium 3
+# from webdriver_manager.chrome import ChromeDriverManager
+
+# driver = webdriver.Chrome(ChromeDriverManager().install())
 
 url = "http://localhost:8000/account/login"
-# path = "C:/Users/ELLAH/Downloads/chromedriver"
-
-# driver = webdriver.Chrome()
-
-driver.get(url)
+# driver.get('url')
+driver = Chrome()
 
 
 
@@ -32,10 +30,10 @@ driver.get(url)
 # //*[@id="password"]
 # /html/body/div/div/div/div[2]/div/div[1]/div/div/form/div[4]
 # //*[@id="app"]/div[1]/div[2]/main/div[3]/button
-username = driver.find_element("xpath", '//*[@id="nicholas"]')
-username.send_keys('****')
-password = driver.find_element("xpath", '//*[@id="bposeats"]')
-password.send_keys('***')
+username = driver.find_element("xpath", '//*[@id="username"]')
+username.send_keys('nicholas')
+password = driver.find_element("xpath", '//*[@id="password"]')
+password.send_keys('bposeats')
 login = driver.find_element("xpath", 
     '/html/body/div/div/div/div[2]/div/div[1]/div/div/form/div[4]')
 login.click()
